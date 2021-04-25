c158e3d52387909134af1c90e9b5705e2ab9dd7b
knagunoori26 <83174722+knagunoori26@users.noreply.github.com>
2021-04-25 11:09:35 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,2 +1,9 @@
 import py_entitymatching as em
 from os.path import join
+
+# 1. Reading Data
+left = em.read_csv_metadata(join('data', "ltable.csv"), key='id')
+right = em.read_csv_metadata(join('data', "rtable.csv"), key='id')
+train = em.read_csv_metadata(join('data', "train.csv"))
+train['id'] = range(0, len(train))
+em.set_key(A, 'id')
